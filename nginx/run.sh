82f2d5a89b13cc250f1dc4fbc6efd9dec7ca6168
48763 <future.starshine@gmail.com>
2018-11-01 15:39:29 +0800

    Update

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,7 +1,7 @@
 DOCKER_CONTAINER_HASH=$(docker ps -a -f "name=nginx" -q)
 
-docker stop DOCKER_CONTAINER_HASH
-docker rm DOCKER_CONTAINER_HASH
+docker stop $DOCKER_CONTAINER_HASH
+docker rm $DOCKER_CONTAINER_HASH
 
 docker run --name nginx -v /var/log/nginx:/var/log/nginx \
         -p 80:80 \
